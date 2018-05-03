51fe5b76c574fe96bb9b1ff3f14ec10d4a486e6f
Ticktock <smu@ticktock.local>
2018-05-03 22:23:49 +0800

    basic

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-var monkey = "hello";
+class Twitter extends React.Component {
+
+    render() {
+        return (
+          <div>
+          <img src={this.props.profile_image} />
+          <h6><a href={this.props.links}>{this.props.username}</a></h6>
+          <p>{this.props.text}</p>
+          <hr />
+          </div>
+
+        );
+    }
+}
+
+class Tweeting extends React.Component {
+
+    render() {
+        let tweetElements = this.props.tweets.map( (tweet, index) => {
+                              return <Twitter text={tweet.text} username={tweet.user.name} profile_image={tweet.user.profile_image_url}
+                              links={tweet.user.entities.url.urls[0].expanded_url} ></Twitter>;
+                            });
+        return (
+          <ul>
+            {tweetElements}
+          </ul>
+        );
+    }
+}
+
+
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
+    <Tweeting tweets={tweets} />,
     document.getElementById('root')
 );
